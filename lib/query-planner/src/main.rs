200d54996fdf2f1bbd87f4787085c0216dd6548e
Dotan Simha <dotansimha@gmail.com>
2025-01-26 15:51:34 +0200

    use workspace

--- lib/query-planner/src/main.rs
+++ lib/query-planner/src/main.rs
@@ -8,17 +8,18 @@ mod node;
 mod operation_advisor;
 mod supergraph;
 
-use std::fs;
+use std::{fs, path::Path};
 
 use graphql_parser_hive_fork::parse_query;
 use operation_advisor::OperationAdvisor;
 use supergraph::SupergraphIR;
 
 fn main() {
-    let supergraph_sdl = fs::read_to_string("fixture/dotan.supergraph.graphql")
+    let current_dir = Path::new(file!()).parent().unwrap().parent().unwrap();
+    let supergraph_sdl = fs::read_to_string(current_dir.join("./fixture/dotan.supergraph.graphql"))
         .expect("Unable to read supergraph.graphql");
 
-    let operation = fs::read_to_string("fixture/dotan.operation.graphql")
+    let operation = fs::read_to_string(current_dir.join("./fixture/dotan.operation.graphql"))
         .expect("Unable to read dotan.operation.graphql");
     let parsed_operation = parse_query(&operation).unwrap().into_static();
 
