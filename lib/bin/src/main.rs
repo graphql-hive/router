377d0b65664bf8910d76eaa054f956e23cef815c
Dotan Simha <dotansimha@gmail.com>
2025-02-04 17:06:51 +0200

    added bin utility

---
+++ lib/bin/src/main.rs
@@ -0,0 +1,39 @@
+use std::env;
+use std::process;
+
+use query_planner::operation_advisor::OperationAdvisor;
+use query_planner::parse_schema;
+use query_planner::supergraph::SupergraphMetadata;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        eprintln!("Usage: query-planner <command> <supergraph_path>");
+        process::exit(1);
+    }
+
+    match args[1].as_str() {
+        "consumer_schema" => process_consumer_schema(&args[2]),
+        "satisfiability_graph" => process_satisfiability_graph(&args[2]),
+        _ => {
+            eprintln!("Unknown command. Available commands: consumer_graph, satisfiability_graph");
+            process::exit(1);
+        }
+    }
+}
+
+fn process_consumer_schema(path: &str) {
+    let supergraph_sdl = std::fs::read_to_string(path).expect("Unable to read input file");
+    let parsed_schema = parse_schema(&supergraph_sdl);
+    let advisor = OperationAdvisor::new(SupergraphMetadata::new(&parsed_schema));
+    println!("{}", advisor.consumer_schema.document);
+}
+
+fn process_satisfiability_graph(path: &str) {
+    let supergraph_sdl = std::fs::read_to_string(path).expect("Unable to read input file");
+    let parsed_schema = parse_schema(&supergraph_sdl);
+    let advisor = OperationAdvisor::new(SupergraphMetadata::new(&parsed_schema));
+
+    println!("{}", advisor.graph);
+}
