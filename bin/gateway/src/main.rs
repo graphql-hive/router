b98c57adb366e314c5cf760f72539d66ae613388
Arda TANRIKULU <ardatanrikulu@gmail.com>
2025-05-27 17:59:27 +0300

    Executor - Handle default values on operations (#85)
    * Handle default values on operations
    
    * lets go
    
    * Fix clippy
    
    * Fix spec
    
    * AI comments
    
    * Go

--- bin/gateway/src/main.rs
+++ bin/gateway/src/main.rs
@@ -15,7 +15,7 @@ use query_planner::state::supergraph_state::SupergraphState;
 use query_planner::utils::parsing::parse_operation;
 use query_planner::utils::parsing::parse_schema;
 use serde_json::json;
-use serde_json::Value;
+use serde_json::Value::{self};
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 
@@ -73,27 +73,78 @@ struct ServeData {
     subgraph_endpoint_map: HashMap<String, String>,
 }
 
+fn from_graphql_value_to_serde_value(
+    value: &graphql_parser::query::Value<'static, String>,
+    variables: &Option<HashMap<String, Value>>,
+) -> serde_json::Value {
+    match value {
+        graphql_parser::query::Value::Null => serde_json::Value::Null,
+        graphql_parser::query::Value::Boolean(b) => serde_json::Value::Bool(*b),
+        graphql_parser::query::Value::String(s) => serde_json::Value::String(s.to_string()),
+        graphql_parser::query::Value::Enum(e) => serde_json::Value::String(e.to_string()),
+        // TODO: Handle variable parsing errors here just like in GraphQL-JS
+        graphql_parser::query::Value::Int(n) => serde_json::Value::Number(
+            serde_json::Number::from(n.as_i64().expect("Failed to coerce")),
+        ),
+        graphql_parser::query::Value::Float(n) => {
+            serde_json::Value::Number(serde_json::Number::from_f64(*n).expect("Failed to coerce"))
+        }
+        graphql_parser::query::Value::List(l) => serde_json::Value::Array(
+            l.iter()
+                .map(|v| from_graphql_value_to_serde_value(v, variables))
+                .collect(),
+        ),
+        graphql_parser::query::Value::Object(o) => serde_json::Value::Object(
+            o.iter()
+                .map(|(k, v)| {
+                    (
+                        k.to_string(),
+                        from_graphql_value_to_serde_value(v, variables),
+                    )
+                })
+                .collect(),
+        ),
+        graphql_parser::query::Value::Variable(var_name) => {
+            if let Some(variables_map) = variables {
+                if let Some(value) = variables_map.get(var_name) {
+                    value.clone() // Return the value from the variables map
+                } else {
+                    serde_json::Value::Null // If variable not found, return null
+                }
+            } else {
+                serde_json::Value::Null // If no variables provided, return null
+            }
+        }
+    }
+}
+
 fn collect_variables(
     operation: &OperationDefinition<'static, String>,
     variables: &Option<HashMap<String, Value>>,
 ) -> Option<HashMap<String, Value>> {
-    let mut variable_values = HashMap::new();
     let variable_definitions = match &operation {
-        OperationDefinition::SelectionSet(_) => &vec![],
+        OperationDefinition::SelectionSet(_) => return None,
         OperationDefinition::Query(query) => &query.variable_definitions,
         OperationDefinition::Mutation(mutation) => &mutation.variable_definitions,
         OperationDefinition::Subscription(subscription) => &subscription.variable_definitions,
     };
-    for variable_definition in variable_definitions {
-        let variable_name = variable_definition.name.to_string();
-        if let Some(variable_value) = variables.as_ref().and_then(|v| v.get(&variable_name)) {
-            variable_values.insert(variable_name, variable_value.clone());
-        } else if let Some(default_value) = &variable_definition.default_value {
-            todo!("Not implemented yet: default value: {}", default_value);
-        } else {
-            variable_values.insert(variable_name, Value::Null);
-        }
-    }
+    let variable_values: HashMap<String, Value> = variable_definitions
+        .iter()
+        .filter_map(|variable_definition| {
+            let variable_name = variable_definition.name.to_string();
+            if let Some(variable_value) = variables.as_ref().and_then(|v| v.get(&variable_name)) {
+                return Some((variable_name, variable_value.clone()));
+            }
+            if let Some(default_value) = &variable_definition.default_value {
+                let default_value_coerced =
+                    from_graphql_value_to_serde_value(default_value, variables);
+                if !default_value_coerced.is_null() {
+                    return Some((variable_name, default_value_coerced));
+                }
+            }
+            None
+        })
+        .collect();
     if variable_values.is_empty() {
         None
     } else {
