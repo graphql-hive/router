887a818f1980ea0720a3b5241426cf7ec60bd4e8
Dotan Simha <dotansimha@gmail.com>
2025-06-10 10:58:46 +0300

    chore: move all audits files to `./audits` dir (#144)

--- audits/run-gw.sh
+++ audits/run-gw.sh
@@ -3,8 +3,11 @@ set -e
 
 echo "generating supergraph file for test..."
 npx graphql-federation-audit supergraph --cwd . --test $1
+mv supergraph.graphql fed-audit-supergraph.graphql
 
 export RUST_LOG=debug
 
 echo "running gateway..."
-cargo gateway supergraph.graphql
+
+cd ..
+cargo gateway audits/fed-audit-supergraph.graphql
