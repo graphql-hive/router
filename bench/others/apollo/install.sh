4b8ae1cb8b24ba19446c301a286f5298816394f0
Kamil Kisiela <kamil.kisiela@gmail.com>
2025-06-30 15:10:25 +0200

    Add Gateways benchmark setup to run on-demand

---
+++ bench/others/apollo/install.sh
@@ -0,0 +1,2 @@
+# https://github.com/apollographql/router/releases
+curl -sSL https://router.apollo.dev/download/nix/v2.3.0 | sh
---
+++ bench/others/grafbase/run.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./grafbase-gateway --schema ../../supergraph.graphql --listen-address 0.0.0.0:4000 --log error
