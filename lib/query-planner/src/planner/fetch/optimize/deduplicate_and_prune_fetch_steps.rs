4e81a39de94c5d0a9a1d853aa83b753d6762b117
Dotan Simha <dotansimha@gmail.com>
2025-07-04 11:50:20 +0300

    op

---
+++ lib/query-planner/src/planner/fetch/optimize/deduplicate_and_prune_fetch_steps.rs
@@ -0,0 +1,73 @@
+use petgraph::{
+    graph::{EdgeIndex, NodeIndex},
+    visit::EdgeRef,
+};
+use tracing::{instrument, trace};
+
+use crate::planner::fetch::{
+    error::FetchGraphError, fetch_graph::FetchGraph,
+    optimize::utils::is_reachable_via_alternative_upstream_path,
+};
+
+impl FetchGraph {
+    /// Removes redundant direct dependencies from a FetchStep graph.
+    ///
+    /// ```text
+    /// in:
+    /// A -> C
+    /// A -> B -> ... -> C
+    /// out:
+    /// A -> B -> ... -> C
+    /// ```
+    #[instrument(level = "trace", skip_all)]
+    pub(crate) fn deduplicate_and_prune_fetch_steps(&mut self) -> Result<(), FetchGraphError> {
+        let steps_to_remove: Vec<NodeIndex> = self
+            .step_indices()
+            .filter(|&step_index| {
+                let step = match self.get_step_data(step_index) {
+                    Ok(s) => s,
+                    Err(_) => return false,
+                };
+
+                if !step.output.selection_set.items.is_empty()
+                    && self.parents_of(step_index).next().is_some()
+                {
+                    return false;
+                }
+
+                if self.children_of(step_index).next().is_some() {
+                    return false;
+                }
+
+                trace!("optimization found: remove '{}'", step);
+
+                true
+            })
+            .collect();
+
+        for step_index in steps_to_remove {
+            self.remove_step(step_index);
+        }
+
+        let mut edges_to_remove: Vec<EdgeIndex> = vec![];
+        for step_index in self.step_indices() {
+            for parent_to_step_edge in self.parents_of(step_index) {
+                let direct_parent_index = parent_to_step_edge.source();
+                let child_index = step_index;
+                if is_reachable_via_alternative_upstream_path(
+                    self,
+                    child_index,
+                    direct_parent_index,
+                )? {
+                    edges_to_remove.push(parent_to_step_edge.id());
+                }
+            }
+        }
+
+        for edge_index in edges_to_remove {
+            self.remove_edge(edge_index);
+        }
+
+        Ok(())
+    }
+}
---
+++ lib/query-planner/src/planner/fetch/optimize/merge_siblings.rs
@@ -0,0 +1,104 @@
+use std::collections::{HashMap, VecDeque};
+
+use petgraph::{graph::NodeIndex, Direction};
+use tracing::{instrument, trace};
+
+use crate::planner::fetch::{
+    error::FetchGraphError, fetch_graph::FetchGraph, optimize::utils::perform_fetch_step_merge,
+};
+
+impl FetchGraph {
+    #[instrument(level = "trace", skip_all)]
+    pub(crate) fn merge_siblings(&mut self) -> Result<(), FetchGraphError> {
+        let root_index = self
+            .root_index
+            .ok_or(FetchGraphError::NonSingleRootStep(0))?;
+        // Breadth-First Search (BFS) starting from the root node.
+        let mut queue = VecDeque::from([root_index]);
+
+        while let Some(parent_index) = queue.pop_front() {
+            // Store pairs of sibling nodes that can be merged.
+            // The additional Vec<usize> is an indicator for conflicting field indexes in the 2nd sibling.
+            // If the Vec is empty, it means there are no conflicts.
+            let mut merges_to_perform = Vec::<(NodeIndex, NodeIndex)>::new();
+
+            // HashMap to keep track of node index mappings, especially after merges.
+            // Key: original index, Value: potentially updated index after merges.
+            let mut node_indexes: HashMap<NodeIndex, NodeIndex> = HashMap::new();
+
+            // Sort fetch steps by mutation's field position,
+            // to execute mutations in correct order.
+            let mut siblings_with_pos: Vec<(NodeIndex, Option<usize>)> = self
+                .graph
+                .neighbors_directed(parent_index, Direction::Outgoing)
+                .map(|sibling| {
+                    self.get_step_data(sibling)
+                        .map(|data| (sibling, data.mutation_field_position))
+                })
+                .collect::<Result<_, _>>()?;
+
+            // Sort fetch steps by mutation's field position,
+            // to execute mutations in correct order.
+            siblings_with_pos.sort_by_key(|(_node, pos)| *pos);
+
+            let siblings: Vec<NodeIndex> =
+                siblings_with_pos.into_iter().map(|(idx, _)| idx).collect();
+
+            for (i, sibling_index) in siblings.iter().enumerate() {
+                // Add the current node to the queue for further processing (BFS).
+                queue.push_back(*sibling_index);
+                let current = self.get_step_data(*sibling_index)?;
+
+                // Iterate through the remaining children (siblings) to check for merge possibilities.
+                for other_sibling_index in siblings.iter().skip(i + 1) {
+                    let other_sibling = self.get_step_data(*other_sibling_index)?;
+
+                    trace!(
+                        "checking if [{}] and [{}] can be merged",
+                        sibling_index.index(),
+                        other_sibling_index.index()
+                    );
+
+                    if current.can_merge_siblings(
+                        *sibling_index,
+                        *other_sibling_index,
+                        other_sibling,
+                        self,
+                    ) {
+                        trace!(
+                            "Found siblings optimization: {} <- {}",
+                            sibling_index.index(),
+                            other_sibling_index.index()
+                        );
+                        // Register their original indexes in the map.
+                        node_indexes.insert(*sibling_index, *sibling_index);
+                        node_indexes.insert(*other_sibling_index, *other_sibling_index);
+
+                        merges_to_perform.push((*sibling_index, *other_sibling_index));
+
+                        // Since a merge is possible, move to the next child to avoid redundant checks.
+                        break;
+                    }
+                }
+            }
+
+            for (child_index, other_child_index) in merges_to_perform {
+                // Get the latest indexes for the nodes, accounting for previous merges.
+                let child_index_latest = node_indexes
+                    .get(&child_index)
+                    .ok_or(FetchGraphError::IndexMappingLost)?;
+                let other_child_index_latest = node_indexes
+                    .get(&other_child_index)
+                    .ok_or(FetchGraphError::IndexMappingLost)?;
+
+                perform_fetch_step_merge(*child_index_latest, *other_child_index_latest, self)?;
+
+                // Because `other_child` was merged into `child`,
+                // then everything that was pointing to `other_child`
+                // has to point to the `child`.
+                node_indexes.insert(*other_child_index_latest, *child_index_latest);
+            }
+        }
+        Ok(())
+    }
+}
