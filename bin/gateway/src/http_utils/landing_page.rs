3e40238d8bb54e9a9547d28e475026ec04be2df9
Arda TANRIKULU <ardatanrikulu@gmail.com>
2025-08-01 16:07:10 +0300

    Remove subgraph endpoint map from the landing page

--- bin/gateway/src/http_utils/landing_page.rs
+++ bin/gateway/src/http_utils/landing_page.rs
@@ -1,55 +1,22 @@
-use axum::{extract::State, http::Uri, response::Html};
-use std::sync::Arc;
-
-use crate::shared_state::GatewaySharedState;
+use axum::{http::Uri, response::Html};
 
 static LANDING_PAGE_HTML: &str = include_str!("../../static/landing-page.html");
 static PRODUCT_LOGO_SVG: &str = include_str!("../../static/product_logo.svg");
 
-pub async fn landing_page_handler(
-    State(gateway_shared_state): State<Arc<GatewaySharedState>>,
-    uri: Uri,
-) -> Html<String> {
-    let mut subgraph_html = String::new();
-    subgraph_html.push_str("<section class=\"supergraph-information\">");
-    subgraph_html.push_str("<h3>Supergraph Status: Loaded ✅</h3>");
-    subgraph_html.push_str("<table>");
-    subgraph_html.push_str("<tr><th>Subgraph</th><th>Transport</th><th>Location</th></tr>");
-    for (subgraph_name, subgraph_endpoint) in &gateway_shared_state.subgraph_endpoint_map {
-        subgraph_html.push_str("<tr>");
-        subgraph_html.push_str(&format!("<td>{}</td>", subgraph_name));
-        subgraph_html.push_str(&format!(
-            "<td>{}</td>",
-            if subgraph_endpoint.starts_with("http") {
-                "http"
-            } else {
-                "Unknown"
-            }
-        ));
-        subgraph_html.push_str(&format!(
-            "<td><a href=\"{}\">{}</a></td>",
-            subgraph_endpoint, subgraph_endpoint
-        ));
-        subgraph_html.push_str("</tr>");
-    }
-    subgraph_html.push_str("</table>");
-    subgraph_html.push_str("</section>");
-
+pub async fn landing_page_handler(uri: Uri) -> Html<String> {
     let rendered_html = LANDING_PAGE_HTML
         .replace("__GRAPHIQL_LINK__", "/graphql")
         .replace("__REQUEST_PATH__", uri.path())
-        .replace("__PRODUCT_NAME__", "Hive Gateway RS")
+        .replace("__PRODUCT_NAME__", "Hive Router")
         .replace(
             "__PRODUCT_DESCRIPTION__",
             "A GraphQL Gateway written in Rust",
         )
-        .replace("__PRODUCT_PACKAGE_NAME__", "hive-gateway-rs")
         .replace(
             "__PRODUCT_LINK__",
-            "https://the-guild.dev/graphql/hive/docs/gateway",
+            "https://the-guild.dev/graphql/hive/docs/router",
         )
-        .replace("__PRODUCT_LOGO__", PRODUCT_LOGO_SVG)
-        .replace("__SUBGRAPH_HTML__", &subgraph_html);
+        .replace("__PRODUCT_LOGO__", PRODUCT_LOGO_SVG);
 
     Html(rendered_html)
 }
