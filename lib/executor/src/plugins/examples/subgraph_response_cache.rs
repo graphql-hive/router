8f5ba0e75e153b5aea1dd4ad7fa3c17c09c704a8
Arda TANRIKULU <ardatanrikulu@gmail.com>
2025-10-14 17:30:13 +0300

    Another plugin

--- lib/executor/src/plugins/examples/subgraph_response_cache.rs
+++ lib/executor/src/plugins/examples/subgraph_response_cache.rs
@@ -1,32 +1,30 @@
 use dashmap::DashMap;
 
-use crate::{hooks::on_subgraph_execute::{OnSubgraphExecuteEndPayload, OnSubgraphExecuteStartPayload, SubgraphExecutorResponse, SubgraphResponse}, plugin_trait::{ControlFlow, RouterPlugin}};
+use crate::{executors::dedupe::SharedResponse, hooks::on_subgraph_http_request::{OnSubgraphHttpRequestPayload, OnSubgraphHttpResponsePayload}, plugin_trait::{ControlFlow, RouterPlugin}};
 
-struct SubgraphResponseCachePlugin {
-    cache: DashMap<String, SubgraphResponse<'static>>,
+pub struct SubgraphResponseCachePlugin {
+    cache: DashMap<String, SharedResponse>,
 }
 
 impl RouterPlugin for SubgraphResponseCachePlugin {
-    fn on_subgraph_execute<'exec>(
-            &self, 
-            payload: OnSubgraphExecuteStartPayload<'exec>,
-        ) -> ControlFlow<'exec, OnSubgraphExecuteEndPayload<'exec>> {
+    fn on_subgraph_http_request<'exec>(
+            &'static self, 
+            payload: OnSubgraphHttpRequestPayload<'exec>,
+        ) -> ControlFlow<'exec, OnSubgraphHttpResponsePayload<'exec>> {
         let key = format!(
-            "subgraph_response_cache:{}:{}:{:?}",
-            payload.subgraph_name, payload.execution_request.operation_name.unwrap_or(""), payload.execution_request.variables
+            "subgraph_response_cache:{}:{:?}",
+            payload.execution_request.query, payload.execution_request.variables
         );
         if let Some(cached_response) = self.cache.get(&key) {
-            *payload.response = Some(SubgraphExecutorResponse::RawResponse(cached_response));
-            // Return early with the cached response
+            // Here payload.response is Option
+            // So it is bypassing the actual subgraph request
+            *payload.response = Some(cached_response.clone());
             return ControlFlow::Continue;
-        } else {
-            ControlFlow::OnEnd(Box::new(move |payload: OnSubgraphExecuteEndPayload| {
-                let cacheable = payload.response.errors.is_none_or(|errors| errors.is_empty());
-                if cacheable {
-                    self.cache.insert(key, *payload.response);
-                }
-                ControlFlow::Continue
-            }))
         }
+        ControlFlow::OnEnd(Box::new(move |payload: OnSubgraphHttpResponsePayload| {
+            // Here payload.response is not Option
+            self.cache.insert(key, payload.response.clone());
+            ControlFlow::Continue
+        }))
     }
 }
