6422a234c7d665ba914de111f54740598b231dbb
Kamil Kisiela <kamil.kisiela@gmail.com>
2025-10-23 15:40:28 +0200

    one more test

--- lib/query-planner/src/ast/normalization/mod.rs
+++ lib/query-planner/src/ast/normalization/mod.rs
@@ -1209,4 +1209,84 @@ mod tests {
         ",
         );
     }
+
+    #[test]
+    fn inlining_nested_fragments_with_same_type() {
+        let schema_str =
+            std::fs::read_to_string("./fixture/tests/nested-fragments.supergraph.graphql")
+                .expect("Unable to read supergraph");
+        let schema = parse_schema(&schema_str);
+        let supergraph = SupergraphState::new(&schema);
+
+        insta::assert_snapshot!(
+            &pretty_query(
+                normalize_operation(
+                    &supergraph,
+                    &parse_query(
+                        r#"
+                        query {
+                          results {
+                            __typename
+                            ... on Tshirt {
+                              ...TshirtResult1
+                            }
+                          }
+                        }
+
+                        fragment TshirtResult1 on Tshirt {
+                          ...TshirtResult2
+                        }
+
+                        fragment TshirtResult2 on Tshirt {
+                          id
+                          name
+                          ...on MultipleColor {
+                            colorOptions {
+                              ...ColorDetails
+                            }
+                          }
+                          ...on SingleColor {
+                            colorOption {
+                              ...ColorDetails
+                            }
+                          }
+                        }
+
+                        fragment ColorDetails on TshirtColorOption {
+                          id
+                          color
+                        }
+                  "#,
+                    )
+                    .expect("to parse"),
+                    None,
+                )
+                .unwrap()
+                .to_string()
+            ),
+            @r"
+            query {
+              results {
+                __typename
+                ... on MultipleColor {
+                  id
+                  name
+                  colorOptions {
+                    id
+                    color
+                  }
+                }
+                ... on SingleColor {
+                  id
+                  name
+                  colorOption {
+                    id
+                    color
+                  }
+                }
+              }
+            }
+        "
+        );
+    }
 }
