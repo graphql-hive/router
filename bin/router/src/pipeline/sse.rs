feb4887837c46a14b87dda438b0469c3fbc02779
enisdenjo <badurinadenis@gmail.com>
2025-12-13 07:30:43 +0100

    optimize right

--- bin/router/src/pipeline/sse.rs
+++ bin/router/src/pipeline/sse.rs
@@ -2,6 +2,7 @@ use futures_timer::Delay;
 use futures_util::{FutureExt, Stream, StreamExt};
 use ntex::util::Bytes;
 use std::time::Duration;
+use tokio_util::bytes::BufMut;
 
 // TODO: test this bad boy
 // TODO: not be a quick implementation
@@ -17,9 +18,13 @@ pub fn create_stream(
                 item = input.next() => {
                     match item {
                         Some(resp) => {
-                            match String::from_utf8(resp) {
+                             match std::str::from_utf8(&resp) {
                                 Ok(json_str) => {
-                                    yield Ok(Bytes::from(format!("event: next\ndata: {}\n\n", json_str)));
+                                    let mut sse_event = ntex::util::BytesMut::with_capacity(json_str.len() + 25);
+                                    sse_event.put_slice(b"event: next\ndata: ");
+                                    sse_event.put_slice(json_str.as_bytes());
+                                    sse_event.put_slice(b"\n\n");
+                                    yield Ok(sse_event.freeze());
                                 }
                                 Err(e) => {
                                     yield Err(std::io::Error::new(std::io::ErrorKind::InvalidData, e));
