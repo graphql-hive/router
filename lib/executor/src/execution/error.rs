e374c1256f851842ca1185c2d012cef491fa5df1
Kamil Kisiela <kamil.kisiela@gmail.com>
2026-01-13 12:22:37 +0100

    Avoid clones in impl From<PlanExecutionError> for GraphQLError (#666)
    **Before**
    
    ```rust
    impl From<PlanExecutionError> for GraphQLError {
        fn from(val: PlanExecutionError) -> Self {
            let mut error = GraphQLError::from_message_and_code(val.to_string(), val.error_code());
            
            if let Some(subgraph_name) = val.subgraph_name() {   // Returns &Option<String>
                error = error.add_subgraph_name(subgraph_name);  // Clones via Into<String>
            }
            
            if let Some(affected_path) = val.affected_path() {   // Returns &Option<String>
                error = error.add_affected_path(affected_path);  // Clones via Into<String>
            }
            error
        }
    }
    ```
    
    What happens:
    1. `val.subgraph_name()` returns `&Option<String>`
    2. `add_subgraph_name<TStr: Into<String>>()` expects an `Into<String>`
    3. When Rust sees `&String` -> `Into<String>`, it uses the impl that
    clones the String
    4. Same issue for `affected_path`
    
    We end up with 2 unnecessary heap allocations per error conversion.
    
    **After**
    
    I moved owned values out of the structure, to the `add_subgraph_name`
    and `add_affected_path` methods.
    When Rust sees `Stirng` -> `Into<String>`, it will just use the owned
    String, so no clones.

--- lib/executor/src/execution/error.rs
+++ lib/executor/src/execution/error.rs
@@ -74,10 +74,15 @@ impl PlanExecutionError {
 impl From<PlanExecutionError> for GraphQLError {
     fn from(val: PlanExecutionError) -> Self {
         let mut error = GraphQLError::from_message_and_code(val.to_string(), val.error_code());
-        if let Some(subgraph_name) = val.subgraph_name() {
+
+        // We destructure the context to take ownership of the Option<String> values.
+        // Then we move owned Strings directly into builder methods.
+        // This way we avoid cloning through Into<String> in those methods.
+
+        if let Some(subgraph_name) = val.context.subgraph_name {
             error = error.add_subgraph_name(subgraph_name);
         }
-        if let Some(affected_path) = val.affected_path() {
+        if let Some(affected_path) = val.context.affected_path {
             error = error.add_affected_path(affected_path);
         }
         error
