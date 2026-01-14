dac5b063fce6d32f2cd67d867fd14a27a7f9380b
Kamil Kisiela <kamil.kisiela@gmail.com>
2026-01-14 20:39:29 +0100

    Disable default features for reqwest and use rustls in every crate

--- bin/router/src/main.rs
+++ bin/router/src/main.rs
@@ -5,6 +5,15 @@ static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 #[ntex::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Uses aws as ring is no longer maintained.
+    // The default provider is decided in the main function,
+    // to let hive-router crate users pick their own crypto provider.
+    // The rest of our crates, depends on rustls and relies on consumers
+    // to define the crypto provider.
+    rustls::crypto::aws_lc_rs::default_provider()
+        .install_default()
+        .expect("installing default crypto provider");
+
     match router_entrypoint().await {
         Ok(_) => Ok(()),
         Err(err) => {
