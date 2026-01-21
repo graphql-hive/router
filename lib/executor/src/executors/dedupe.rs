cd0095ef2c26fabdf83d96e6adff58a37a3a6d84
Kamil Kisiela <kamil.kisiela@gmail.com>
2026-01-21 09:48:02 +0100

    Use Xxh3 for request fingerprinting
    Replaced ahash::AHasher with xxhash_rust::xxh3::Xxh3. The previous
    hasher used random seeds, which prevented consistent hashing between
    test runs. Xxh3 provides a stable hash for repeatable results.

--- lib/executor/src/executors/dedupe.rs
+++ lib/executor/src/executors/dedupe.rs
@@ -3,7 +3,8 @@ use bytes::Bytes;
 use hive_router_internal::telemetry::otel::opentelemetry::trace::SpanContext;
 use http::{HeaderMap, Method, StatusCode, Uri};
 use std::collections::BTreeMap;
-use std::hash::{BuildHasher, BuildHasherDefault, Hash, Hasher};
+use std::hash::{BuildHasherDefault, Hash, Hasher};
+use xxhash_rust::xxh3::Xxh3;
 
 #[derive(Debug, Clone)]
 pub struct SharedResponse {
@@ -19,8 +20,12 @@ pub fn request_fingerprint(
     req_headers: &HeaderMap,
     body_bytes: &[u8],
 ) -> u64 {
-    let build_hasher = ABuildHasher::default();
-    let mut hasher = build_hasher.build_hasher();
+    // In e2e tests, we want to be able to compare request fingerprints,
+    // and that requires this function to produce the same hash for the same input,
+    // between test runs (between processes).
+    // We used to have ahash::AHasher, but it was using random seeds every time it was created.
+    // It prevented consistent hashing between test runs.
+    let mut hasher = Xxh3::new();
 
     // BTreeMap to ensure case-insensitivity and consistent order for hashing
     let mut headers = BTreeMap::new();
