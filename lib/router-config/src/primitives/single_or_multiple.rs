72477b5cbf1dc9c51f13da4addcea33578cc505a
Arda TANRIKULU <ardatanrikulu@gmail.com>
2026-01-27 12:09:33 +0300

    Single or multiple

--- lib/router-config/src/primitives/single_or_multiple.rs
+++ lib/router-config/src/primitives/single_or_multiple.rs
@@ -1,7 +1,9 @@
-use schemars::JsonSchema;
+use std::borrow::Cow;
+
+use schemars::{JsonSchema, Schema, SchemaGenerator, json_schema};
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Deserialize, Serialize, JsonSchema)]
+#[derive(Debug, Clone, Deserialize, Serialize)]
 #[serde(untagged)]
 pub enum SingleOrMultiple<T> {
     Single(T),
@@ -14,5 +16,21 @@ impl<T> From<SingleOrMultiple<T>> for Vec<T> {
             SingleOrMultiple::Single(item) => vec![item],
             SingleOrMultiple::Multiple(items) => items,
         }
+    }
+}
+
+impl<T: JsonSchema> JsonSchema for SingleOrMultiple<T> {
+    fn schema_name() -> Cow<'static, str> {
+        format!("SingleOrMultiple<{}>", T::schema_name()).into()
+    }
+
+    fn json_schema(gen: &mut SchemaGenerator) -> Schema {
+        let schema = json_schema!({
+            "anyOf": [
+                gen.subschema_for::<T>(),
+                gen.subschema_for::<Vec<T>>()
+            ]
+        });
+        schema
     }
 }
