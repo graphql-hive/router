bdc399f249d68fedd25111e0eff8403de4b44fb9
enisdenjo <badurinadenis@gmail.com>
2026-02-01 16:16:30 +0100

    semaphore

--- lib/executor/src/executors/ws.rs
+++ lib/executor/src/executors/ws.rs
@@ -1,10 +1,12 @@
 use std::collections::HashMap;
+use std::sync::Arc;
 use std::time::Duration;
 
 use async_trait::async_trait;
 use futures::stream::BoxStream;
 use futures_util::StreamExt;
 use ntex::rt::Arbiter;
+use tokio::sync::Semaphore;
 use tracing::debug;
 
 use crate::executors::common::{SubgraphExecutionRequest, SubgraphExecutor};
@@ -13,20 +15,22 @@ use crate::executors::websocket_client::{connect, WsClient};
 use crate::response::subgraph_response::SubgraphResponse;
 
 pub struct WsSubgraphExecutor {
+    arbiter: Arbiter,
     subgraph_name: String,
     endpoint: http::Uri,
-    arbiter: Arbiter,
+    semaphore: Arc<Semaphore>,
 }
 
 impl WsSubgraphExecutor {
-    pub fn new(subgraph_name: String, endpoint: http::Uri) -> Self {
+    pub fn new(subgraph_name: String, endpoint: http::Uri, semaphore: Arc<Semaphore>) -> Self {
         Self {
-            subgraph_name,
-            endpoint,
             // each executors its own arbiter because the WsClient is not sync+send compatible,
             // so we instead spawn a dedicated arbiter (maps to one OS thread) to run all websocket
             // connection tasks for this executor
             arbiter: Arbiter::new(),
+            subgraph_name,
+            endpoint,
+            semaphore,
         }
     }
 }
@@ -52,6 +56,10 @@ impl SubgraphExecutor for WsSubgraphExecutor {
         execution_request: SubgraphExecutionRequest<'a>,
         _timeout: Option<Duration>,
     ) -> SubgraphResponse<'a> {
+        // This unwrap is safe because the semaphore is never closed during the application's lifecycle.
+        // `acquire()` only fails if the semaphore is closed, so this will always return `Ok`.
+        let _permit = self.semaphore.acquire().await.unwrap();
+
         let endpoint = self.endpoint.clone();
         let subgraph_name = self.subgraph_name.clone();
 
@@ -153,6 +161,10 @@ impl SubgraphExecutor for WsSubgraphExecutor {
         execution_request: SubgraphExecutionRequest<'a>,
         _timeout: Option<Duration>,
     ) -> BoxStream<'static, SubgraphResponse<'static>> {
+        // This unwrap is safe because the semaphore is never closed during the application's lifecycle.
+        // `acquire()` only fails if the semaphore is closed, so this will always return `Ok`.
+        let _permit = self.semaphore.acquire().await.unwrap();
+
         let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel::<SubgraphResponse<'static>>();
 
         let endpoint = self.endpoint.clone();
