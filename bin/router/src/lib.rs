909a690682cf508ee5918b31175c91dda970a981
Arda TANRIKULU <ardatanrikulu@gmail.com>
2026-02-26 14:02:31 +0300

    fix(router): add missing `record_response` in error response handler

--- bin/router/src/lib.rs
+++ bin/router/src/lib.rs
@@ -111,6 +111,9 @@ async fn graphql_endpoint_handler(
         .telemetry_context
         .extract_context(&HeaderExtractor(request.headers()));
     let root_http_request_span = HttpServerRequestSpan::from_request(&request);
+    request
+        .extensions_mut()
+        .insert(root_http_request_span.clone());
     let _ = root_http_request_span.set_parent(parent_ctx);
 
     async {
