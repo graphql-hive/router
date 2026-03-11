c83a0f974d05653c180bc043f455d24cc8e19739
Denis Badurina <denis@denelop.com>
2026-03-11 17:43:58 +0100

    Revert "ok check parts"
    This reverts commit 25d0f4616986ea661defc093fcd0326b8cbbf01f.

--- e2e/src/subscriptions.rs
+++ e2e/src/subscriptions.rs
@@ -1176,15 +1176,23 @@ mod subscriptions_e2e_tests {
             )
             .await;
 
-        let body = res.string_body().await;
+        let body = res.body().await.unwrap();
+        let body_str = std::str::from_utf8(&body).unwrap();
 
-        // emitted at least one event
-        assert!(body.contains(r#"data: {"data":{"reviewAdded":{"id":"1"}}}"#));
+        assert_snapshot!(body_str, @r#"
+        event: next
+        data: {"data":{"reviewAdded":{"id":"1"}}}
 
-        // kicked off client
-        assert!(body.contains(r#"data: {"data":null,"errors":[{"message":"Failed to execute request to subgraph","extensions":{"code":"SUBGRAPH_SUBSCRIPTION_STREAM_ERROR","serviceName":"reviews"}}]}"#));
+        event: next
+        data: {"data":{"reviewAdded":{"id":"2"}}}
 
-        // completed stream
-        assert!(body.contains("event: complete"));
+        event: next
+        data: {"data":{"reviewAdded":{"id":"3"}}}
+
+        event: next
+        data: {"data":null,"errors":[{"message":"Failed to execute request to subgraph","extensions":{"code":"SUBGRAPH_SUBSCRIPTION_STREAM_ERROR","serviceName":"reviews"}}]}
+
+        event: complete
+        "#);
     }
 }
