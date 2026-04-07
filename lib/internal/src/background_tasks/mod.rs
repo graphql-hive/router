8ae99327dc249e2cea1c67cb255bf3ade4fc6603
Denis Badurina <denis@denelop.com>
2026-04-07 15:31:24 +0200

    bump and no wargnings

--- lib/internal/src/background_tasks/mod.rs
+++ lib/internal/src/background_tasks/mod.rs
@@ -1,5 +1,5 @@
 use async_trait::async_trait;
-use ntex::rt::Arbiter;
+use ntex::rt::spawn;
 use std::future::Future;
 pub use tokio_util::sync::CancellationToken;
 use tracing::info;
@@ -12,7 +12,6 @@ pub trait BackgroundTask: Send + Sync {
 
 pub struct BackgroundTasksManager {
     cancellation_token: CancellationToken,
-    arbiter: Arbiter,
 }
 
 impl Default for BackgroundTasksManager {
@@ -25,7 +24,6 @@ impl BackgroundTasksManager {
     pub fn new() -> Self {
         Self {
             cancellation_token: CancellationToken::new(),
-            arbiter: Arbiter::new(),
         }
     }
 
@@ -35,7 +33,7 @@ impl BackgroundTasksManager {
     {
         info!("registering background task: {}", task.id());
         let child_token = self.cancellation_token.clone();
-        self.arbiter.spawn(async move {
+        spawn(async move {
             task.run(child_token).await;
         });
     }
@@ -44,13 +42,13 @@ impl BackgroundTasksManager {
     where
         F: Future<Output = ()> + Send + 'static,
     {
-        self.arbiter.spawn(f);
+        spawn(f);
     }
 
     pub fn shutdown(&self) {
         info!("shutdown triggered, stopping all background tasks...");
         self.cancellation_token.cancel();
-        self.arbiter.stop();
+        // rt spawned tasks run on the current runtime, so cancelling the token is sufficient to stop them
         info!("all background tasks have been shut down gracefully.");
     }
 }
