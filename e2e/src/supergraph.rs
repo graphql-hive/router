3afe2cccd05446afb0b5c0de34ec8e92d5e0288e
Denis Badurina <denis@denelop.com>
2026-04-08 16:47:41 +0200

    poll faster because subgraph delay

--- e2e/src/supergraph.rs
+++ e2e/src/supergraph.rs
@@ -212,7 +212,7 @@ mod supergraph_e2e_tests {
                   source: hive
                   endpoint: http://{host}/supergraph
                   key: dummy_key
-                  poll_interval: 300ms
+                  poll_interval: 100ms
                 "#,
             ))
             .build()
