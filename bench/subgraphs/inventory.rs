d9a81ff9bf2660004fb2b14389deaf260f38a1b1
Kamil Kisiela <kamil.kisiela@gmail.com>
2026-05-07 15:55:40 +0200

    Update inventory.rs

--- bench/subgraphs/inventory.rs
+++ bench/subgraphs/inventory.rs
@@ -99,24 +99,18 @@ impl Query {
     #[graphql(entity)]
     async fn find_product_by_id(
         &self,
-        upc: ID,
         #[graphql(key)] upc: ID,
         price: Option<i64>,
         weight: Option<i64>,
-    ) -> Product {
     ) -> Option<Product> {
         let product = INVENTORY
             .iter()
-            .find(|product| product.upc == upc.to_string())
-            .unwrap();
-        Product {
             .find(|product| product.upc == upc.to_string())?;
 
         Some(Product {
             price,
             weight,
             ..product.clone()
-        }
         })
     }
 }
