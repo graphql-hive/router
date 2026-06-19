6e88fc4d7c50abec868cebe84df7b384f9e9d03e
enisdenjo <badurinadenis@gmail.com>
2026-06-19 22:56:00 +0200

    lol

--- e2e/src/lib.rs
+++ e2e/src/lib.rs
@@ -1,11 +1,11 @@
 #[cfg(test)]
-mod cache_control;
-#[cfg(test)]
 mod authorization_directives_filter;
 #[cfg(test)]
 mod authorization_directives_reject;
 #[cfg(test)]
 mod body_limit;
+#[cfg(test)]
+mod cache_control;
 #[cfg(test)]
 mod circuit_breaker;
 #[cfg(test)]
