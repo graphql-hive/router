345efa45eb58e1c0723d9b9dd51d1b189c17f48f
Kamil Kisiela <kamil.kisiela@gmail.com>
2026-07-17 13:04:05 +0200

    No macro

--- lib/graphql-tools/src/validation/rules/rule.rs
+++ lib/graphql-tools/src/validation/rules/rule.rs
@@ -22,114 +22,383 @@ impl<'doc> ValidationVisitors<'doc> {
     }
 }
 
-macro_rules! forward_to_visitors {
-    ($method:ident($argument:ident: $argument_type:ty)) => {
-        fn $method(
-            &mut self,
-            context: &mut OperationVisitorContext<'doc>,
-            errors: &mut ValidationErrorContext,
-            $argument: $argument_type,
-        ) {
-            for visitor in &mut self.visitors {
-                visitor.$method(context, errors, $argument);
-            }
-        }
-    };
-}
-
 impl<'doc> OperationVisitor<'doc, ValidationErrorContext> for ValidationVisitors<'doc> {
-    forward_to_visitors!(enter_document(document: &'doc Document));
-    forward_to_visitors!(leave_document(document: &Document));
+    fn enter_document(
+        &mut self,
+        context: &mut OperationVisitorContext<'doc>,
+        errors: &mut ValidationErrorContext,
+        document: &'doc Document,
+    ) {
+        for visitor in &mut self.visitors {
+            visitor.enter_document(context, errors, document);
+        }
+    }
 
-    forward_to_visitors!(
-        enter_operation_definition(operation: &'doc OperationDefinition)
-    );
-    forward_to_visitors!(
-        leave_operation_definition(operation: &OperationDefinition)
-    );
+    fn leave_document(
+        &mut self,
+        context: &mut OperationVisitorContext<'doc>,
+        errors: &mut ValidationErrorContext,
+        document: &Document,
+    ) {
+        for visitor in &mut self.visitors {
+            visitor.leave_document(context, errors, document);
+        }
+    }
 
-    forward_to_visitors!(
-        enter_fragment_definition(fragment: &'doc FragmentDefinition)
-    );
-    forward_to_visitors!(
-        leave_fragment_definition(fragment: &FragmentDefinition)
-    );
+    fn enter_operation_definition(
+        &mut self,
+        context: &mut OperationVisitorContext<'doc>,
+        errors: &mut ValidationErrorContext,
+        operation: &'doc OperationDefinition,
+    ) {
+        for visitor in &mut self.visitors {
+            visitor.enter_operation_definition(context, errors, operation);
+        }
+    }
 
-    forward_to_visitors!(
-        enter_variable_definition(variable: &'doc VariableDefinition)
-    );
-    forward_to_visitors!(
-        leave_variable_definition(variable: &VariableDefinition)
-    );
+    fn leave_operation_definition(
+        &mut self,
+        context: &mut OperationVisitorContext<'doc>,
+        errors: &mut ValidationErrorContext,
+        operation: &OperationDefinition,
+    ) {
+        for visitor in &mut self.visitors {
+            visitor.leave_operation_definition(context, errors, operation);
+        }
+    }
 
-    forward_to_visitors!(enter_directive(directive: &Directive));
-    forward_to_visitors!(leave_directive(directive: &Directive));
+    fn enter_fragment_definition(
+        &mut self,
+        context: &mut OperationVisitorContext<'doc>,
+        errors: &mut ValidationErrorContext,
+        fragment: &'doc FragmentDefinition,
+    ) {
+        for visitor in &mut self.visitors {
+            visitor.enter_fragment_definition(context, errors, fragment);
+        }
+    }
 
-    forward_to_visitors!(enter_argument(argument: &'doc (String, Value)));
-    forward_to_visitors!(leave_argument(argument: &(String, Value)));
+    fn leave_fragment_definition(
+        &mut self,
+        context: &mut OperationVisitorContext<'doc>,
+        errors: &mut ValidationErrorContext,
+        fragment: &FragmentDefinition,
+    ) {
+        for visitor in &mut self.visitors {
+            visitor.leave_fragment_definition(context, errors, fragment);
+        }
+    }
 
-    forward_to_visitors!(
-        enter_selection_set(selection_set: &'doc SelectionSet)
-    );
-    forward_to_visitors!(leave_selection_set(selection_set: &SelectionSet));
+    fn enter_variable_definition(
+        &mut self,
+        context: &mut OperationVisitorContext<'doc>,
+        errors: &mut ValidationErrorContext,
+        variable: &'doc VariableDefinition,
+    ) {
+        for visitor in &mut self.visitors {
+            visitor.enter_variable_definition(context, errors, variable);
+        }
+    }
 
-    forward_to_visitors!(enter_field(field: &Field));
-    forward_to_visitors!(leave_field(field: &Field));
+    fn leave_variable_definition(
+        &mut self,
+        context: &mut OperationVisitorContext<'doc>,
+        errors: &mut ValidationErrorContext,
+        variable: &VariableDefinition,
+    ) {
+        for visitor in &mut self.visitors {
+            visitor.leave_variable_definition(context, errors, variable);
+        }
+    }
 
-    forward_to_visitors!(
-        enter_fragment_spread(fragment_spread: &'doc FragmentSpread)
-    );
-    forward_to_visitors!(
-        leave_fragment_spread(fragment_spread: &FragmentSpread)
-    );
+    fn enter_directive(
+        &mut self,
+        context: &mut OperationVisitorContext<'doc>,
+        errors: &mut ValidationErrorContext,
+        directive: &Directive,
+    ) {
+        for visitor in &mut self.visitors {
+            visitor.enter_directive(context, errors, directive);
+        }
+    }
 
-    forward_to_visitors!(
-        enter_inline_fragment(inline_fragment: &InlineFragment)
-    );
-    forward_to_visitors!(
-        leave_inline_fragment(inline_fragment: &InlineFragment)
-    );
+    fn leave_directive(
+        &mut self,
+        context: &mut OperationVisitorContext<'doc>,
+        errors: &mut ValidationErrorContext,
+        directive: &Directive,
+    ) {
+        for visitor in &mut self.visitors {
+            visitor.leave_directive(context, errors, directive);
+        }
+    }
 
-    // Unit is Copy, so the regular forwarding macro handles it.
-    forward_to_visitors!(enter_null_value(value: ()));
-    forward_to_visitors!(leave_null_value(value: ()));
+    fn enter_argument(
+        &mut self,
+        context: &mut OperationVisitorContext<'doc>,
+        errors: &mut ValidationErrorContext,
+        argument: &'doc (String, Value),
+    ) {
+        for visitor in &mut self.visitors {
+            visitor.enter_argument(context, errors, argument);
+        }
+    }
 
-    forward_to_visitors!(enter_scalar_value(value: &Value));
-    forward_to_visitors!(leave_scalar_value(value: &Value));
+    fn leave_argument(
+        &mut self,
+        context: &mut OperationVisitorContext<'doc>,
+        errors: &mut ValidationErrorContext,
+        argument: &(String, Value),
+    ) {
+        for visitor in &mut self.visitors {
+            visitor.leave_argument(context, errors, argument);
+        }
+    }
 
-    forward_to_visitors!(enter_enum_value(value: &String));
-    forward_to_visitors!(leave_enum_value(value: &String));
+    fn enter_selection_set(
+        &mut self,
+        context: &mut OperationVisitorContext<'doc>,
+        errors: &mut ValidationErrorContext,
+        selection_set: &'doc SelectionSet,
+    ) {
+        for visitor in &mut self.visitors {
+            visitor.enter_selection_set(context, errors, selection_set);
+        }
+    }
 
-    forward_to_visitors!(
-        enter_variable_value(variable_name: &'doc str)
-    );
-    forward_to_visitors!(
-        leave_variable_value(variable_name: &String)
-    );
+    fn leave_selection_set(
+        &mut self,
+        context: &mut OperationVisitorContext<'doc>,
+        errors: &mut ValidationErrorContext,
+        selection_set: &SelectionSet,
+    ) {
+        for visitor in &mut self.visitors {
+            visitor.leave_selection_set(context, errors, selection_set);
+        }
+    }
 
-    forward_to_visitors!(enter_list_value(values: &Vec<Value>));
-    forward_to_visitors!(leave_list_value(values: &Vec<Value>));
+    fn enter_field(
+        &mut self,
+        context: &mut OperationVisitorContext<'doc>,
+        errors: &mut ValidationErrorContext,
+        field: &Field,
+    ) {
+        for visitor in &mut self.visitors {
+            visitor.enter_field(context, errors, field);
+        }
+    }
 
-    forward_to_visitors!(
-        enter_object_value(fields: &[(String, Value)])
-    );
-    forward_to_visitors!(
-        leave_object_value(fields: &[(String, Value)])
-    );
+    fn leave_field(
+        &mut self,
+        context: &mut OperationVisitorContext<'doc>,
+        errors: &mut ValidationErrorContext,
+        field: &Field,
+    ) {
+        for visitor in &mut self.visitors {
+            visitor.leave_field(context, errors, field);
+        }
+    }
 
-    forward_to_visitors!(
-        enter_object_field(field: &(String, Value))
-    );
-    forward_to_visitors!(
-        leave_object_field(field: &(String, Value))
-    );
+    fn enter_fragment_spread(
+        &mut self,
+        context: &mut OperationVisitorContext<'doc>,
+        errors: &mut ValidationErrorContext,
+        fragment_spread: &'doc FragmentSpread,
+    ) {
+        for visitor in &mut self.visitors {
+            visitor.enter_fragment_spread(context, errors, fragment_spread);
+        }
+    }
+
+    fn leave_fragment_spread(
+        &mut self,
+        context: &mut OperationVisitorContext<'doc>,
+        errors: &mut ValidationErrorContext,
+        fragment_spread: &FragmentSpread,
+    ) {
+        for visitor in &mut self.visitors {
+            visitor.leave_fragment_spread(context, errors, fragment_spread);
+        }
+    }
+
+    fn enter_inline_fragment(
+        &mut self,
+        context: &mut OperationVisitorContext<'doc>,
+        errors: &mut ValidationErrorContext,
+        inline_fragment: &InlineFragment,
+    ) {
+        for visitor in &mut self.visitors {
+            visitor.enter_inline_fragment(context, errors, inline_fragment);
+        }
+    }
+
+    fn leave_inline_fragment(
+        &mut self,
+        context: &mut OperationVisitorContext<'doc>,
+        errors: &mut ValidationErrorContext,
+        inline_fragment: &InlineFragment,
+    ) {
+        for visitor in &mut self.visitors {
+            visitor.leave_inline_fragment(context, errors, inline_fragment);
+        }
+    }
+
+    fn enter_null_value(
+        &mut self,
+        context: &mut OperationVisitorContext<'doc>,
+        errors: &mut ValidationErrorContext,
+        value: (),
+    ) {
+        for visitor in &mut self.visitors {
+            visitor.enter_null_value(context, errors, value);
+        }
+    }
+
+    fn leave_null_value(
+        &mut self,
+        context: &mut OperationVisitorContext<'doc>,
+        errors: &mut ValidationErrorContext,
+        value: (),
+    ) {
+        for visitor in &mut self.visitors {
+            visitor.leave_null_value(context, errors, value);
+        }
+    }
+
+    fn enter_scalar_value(
+        &mut self,
+        context: &mut OperationVisitorContext<'doc>,
+        errors: &mut ValidationErrorContext,
+        value: &Value,
+    ) {
+        for visitor in &mut self.visitors {
+            visitor.enter_scalar_value(context, errors, value);
+        }
+    }
+
+    fn leave_scalar_value(
+        &mut self,
+        context: &mut OperationVisitorContext<'doc>,
+        errors: &mut ValidationErrorContext,
+        value: &Value,
+    ) {
+        for visitor in &mut self.visitors {
+            visitor.leave_scalar_value(context, errors, value);
+        }
+    }
+
+    fn enter_enum_value(
+        &mut self,
+        context: &mut OperationVisitorContext<'doc>,
+        errors: &mut ValidationErrorContext,
+        value: &String,
+    ) {
+        for visitor in &mut self.visitors {
+            visitor.enter_enum_value(context, errors, value);
+        }
+    }
+
+    fn leave_enum_value(
+        &mut self,
+        context: &mut OperationVisitorContext<'doc>,
+        errors: &mut ValidationErrorContext,
+        value: &String,
+    ) {
+        for visitor in &mut self.visitors {
+            visitor.leave_enum_value(context, errors, value);
+        }
+    }
+
+    fn enter_variable_value(
+        &mut self,
+        context: &mut OperationVisitorContext<'doc>,
+        errors: &mut ValidationErrorContext,
+        variable_name: &'doc str,
+    ) {
+        for visitor in &mut self.visitors {
+            visitor.enter_variable_value(context, errors, variable_name);
+        }
+    }
+
+    fn leave_variable_value(
+        &mut self,
+        context: &mut OperationVisitorContext<'doc>,
+        errors: &mut ValidationErrorContext,
+        variable_name: &String,
+    ) {
+        for visitor in &mut self.visitors {
+            visitor.leave_variable_value(context, errors, variable_name);
+        }
+    }
+
+    fn enter_list_value(
+        &mut self,
+        context: &mut OperationVisitorContext<'doc>,
+        errors: &mut ValidationErrorContext,
+        values: &Vec<Value>,
+    ) {
+        for visitor in &mut self.visitors {
+            visitor.enter_list_value(context, errors, values);
+        }
+    }
+
+    fn leave_list_value(
+        &mut self,
+        context: &mut OperationVisitorContext<'doc>,
+        errors: &mut ValidationErrorContext,
+        values: &Vec<Value>,
+    ) {
+        for visitor in &mut self.visitors {
+            visitor.leave_list_value(context, errors, values);
+        }
+    }
+
+    fn enter_object_value(
+        &mut self,
+        context: &mut OperationVisitorContext<'doc>,
+        errors: &mut ValidationErrorContext,
+        fields: &[(String, Value)],
+    ) {
+        for visitor in &mut self.visitors {
+            visitor.enter_object_value(context, errors, fields);
+        }
+    }
+
+    fn leave_object_value(
+        &mut self,
+        context: &mut OperationVisitorContext<'doc>,
+        errors: &mut ValidationErrorContext,
+        fields: &[(String, Value)],
+    ) {
+        for visitor in &mut self.visitors {
+            visitor.leave_object_value(context, errors, fields);
+        }
+    }
+
+    fn enter_object_field(
+        &mut self,
+        context: &mut OperationVisitorContext<'doc>,
+        errors: &mut ValidationErrorContext,
+        field: &(String, Value),
+    ) {
+        for visitor in &mut self.visitors {
+            visitor.enter_object_field(context, errors, field);
+        }
+    }
+
+    fn leave_object_field(
+        &mut self,
+        context: &mut OperationVisitorContext<'doc>,
+        errors: &mut ValidationErrorContext,
+        field: &(String, Value),
+    ) {
+        for visitor in &mut self.visitors {
+            visitor.leave_object_field(context, errors, field);
+        }
+    }
 }
 
 pub trait ValidationRule: Send + Sync {
-    /// Creates a fresh visitor for this validation run.
-    ///
-    /// Visitors hold per-document state, while rules are shared by validation plans.
-    /// Rules that require a custom document walk can use `validate` instead.
     fn visitor<'doc>(&self) -> ValidationVisitor<'doc>;
     fn error_code(&self) -> &'static str;
 }
