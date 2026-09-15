46a5c462649a6c55909b9761f3acefecc8787aa5
Denis Badurina <denis.badurina@wealthsimple.com>
2026-09-15 13:31:32 +0200

    typo

--- bin/router/src/telemetry/traces/spans/tests.rs
+++ bin/router/src/telemetry/traces/spans/tests.rs
@@ -848,7 +848,8 @@ fn test_graphql_operation_span() {
         span.record_error_count(3);
         layer.assert_recorded_value(&span, attributes::HIVE_GRAPHQL_ERROR_COUNT, "3");
         layer.assert_recorded_value(&span, attributes::OTEL_STATUS_CODE, "Error");
-        // individual error types are recorded on events instead of inventing one for the operation
+        // individual error types are recorded on events instead of inventind
+        // one for the operation
         layer.assert_not_recorded(&span, attributes::ERROR_TYPE);
 
         span.record_errors(|| {
